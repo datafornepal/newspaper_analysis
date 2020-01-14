5a7a2fcc4657a5f3309ebb99b4ce9cf87cf636fd
Ayush Subedi <ayush.subedi@gmail.com>
2020-01-14 19:31:41 +0545

    Add new methodology for analysis with updated keywords

--- newspaper/routes.py
+++ newspaper/routes.py
@@ -7,6 +7,12 @@ from datetime import date
 import glob, os
 import plotly
 import plotly.graph_objects as go
+import re
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+
+
+stop_words = set(stopwords.words('english')) 
 
 links = [
  '/about-town',
@@ -37,28 +43,149 @@ links = [
  '/travel',
  '/world']
 
-data_level1 = [' data ',
- ' data literacy ',
- ' geospatial data ',
- ' household survey ',
- ' information ',
- ' population census ',
- ' record ',
- ' research ',
- ' statistics ',
- ' study ']
+# // TWO WORDS
+# data_level1 = [
+
+#  ' source of data ',
+#  ' data source ',
+#  ' data analysis ',
+#  ' data literacy ',
+#  ' geospatial data ',
+#  ' household survey ', 
+#  ' population census ',
+# ]
+
+# data_level1 = [' data ',
+#  ' data literacy ',
+#  ' geospatial data ',
+#  ' household survey ',
+#  ' information ',
+#  ' population census ',
+#  ' record ',
+#  ' research ',
+#  ' statistics ',
+#  ' study ']
+
+data_level1 = ['arabl land',
+ 'avail data',
+ 'bureau statist',
+ 'busi survey',
+ 'cens publi',
+ 'cens pop',
+ 'children employ',
+ 'civil registr',
+ 'collect method',
+ 'commerci export',
+ 'complet rate',
+ 'consum electr',
+ 'consum energ',
+ 'data access',
+ 'data collect',
+ 'data compil',
+ 'data entri',
+ 'data manag',
+ 'data releas',
+ 'data standard',
+ 'data user',
+ 'demograph data',
+ 'densit popul',
+ 'develop data',
+ 'difusion dat',
+ 'direct statist',
+ 'disaggreg data',
+ 'electr access',
+ 'electr consumpt',
+ 'energi consumpt',
+ 'establish survey',
+ 'exchang rate',
+ 'extern debt',
+ 'fertil rate',
+ 'food import',
+ 'food product',
+ 'gender gap',
+ 'govern debt',
+ 'govern statist',
+ 'gross domest',
+ 'gross nation',
+ 'health expenditur',
+ 'health survey',
+ 'import marchandis',
+ 'improv data',
+ 'improv statist',
+ 'indic measur',
+ 'indic preci',
+ 'inflat rate',
+ 'institut statist',
+ 'interest payment',
+ 'intern tourism',
+ 'irrig land',
+ 'land use',
+ 'life expect',
+ 'livestock product',
+ 'merchandis export',
+ 'merchandis trade',
+ 'model statist',
+ 'mortal rate',
+ 'multilater debt',
+ 'nation account',
+ 'nation statist',
+ 'nation survey',
+ 'national brut',
+ 'national statist',
+ 'open data',
+ 'part revenus',
+ 'pay gap',
+ 'popul census',
+ 'popul growth',
+ 'popul rate',
+ 'price index',
+ 'produccion aliment',
+ 'purchas power',
+ 'qualiti data',
+ 'receit fiscal',
+ 'releas data',
+ 'revenu fiscal',
+ 'rural popul',
+ 'servic export',
+ 'statist agenc',
+ 'statist author',
+ 'statist avail',
+ 'statist committe',
+ 'statist data',
+ 'statist depart',
+ 'statist national',
+ 'statist offic',
+ 'statist servic',
+ 'statist studi',
+ 'survey catalogu',
+ 'tax payment',
+ 'tax revenu',
+ 'trade balanc',
+ 'unemploy rate',
+ 'use data',
+ 'water suppli',
+ 'youth unemploy']
 
 data_level2 = [' CPI ', ' GDP ', ' GNP ', ' HDI ', ' WDI ']
 
 data_level3 = [' correlation ', ' regression ', ' sample size ']
 
 def level1_count(article):
-    count_list = []
+    word_tokens = word_tokenize(article.lower().rstrip()) 
+    filtered_sentence = [w for w in word_tokens if not w in stop_words] 
+    
+    for w in word_tokens: 
+        if w not in stop_words: 
+            filtered_sentence.append(w) 
+
+    article = ' '.join(filtered_sentence)  
+    keyword_list = []
     for word in data_level1:
-        word_count = article.count(word)
-        if word_count > 0:
-            count_list.append({word:word_count})
-    return count_list
+        search_ = (r"\b"+word.split()[0]+r"[a-zA-Z]*\s\b"+word.split()[1]+"[a-zA-Z]*")
+        if re.search(search_, article):
+            keyword_list.append(word)
+    return keyword_list
+
 
 def level2_count(article):
     count_list = []
