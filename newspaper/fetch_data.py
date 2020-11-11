23d214713664f340e143c5be6549fec5367b2030
Ayush Subedi <ayush.subedi@gmail.com>
2020-11-11 17:57:35 +0545

    remove print statements and format

--- newspaper/fetch_data.py
+++ newspaper/fetch_data.py
@@ -2,86 +2,278 @@ from bs4 import BeautifulSoup
 import feedparser
 from datetime import date
 import pandas as pd
-import re  
-from nltk.corpus import stopwords 
-from nltk.tokenize import word_tokenize 
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
-newspapers = [('https://thehimalayantimes.com/feed/', 'ht'), 
-              ('https://english.onlinekhabar.com/feed', 'ok'),
-              ('https://www.nepalitimes.com/feed/', 'nt'),
-              ('https://kathmandutribune.com/feed/', 'kt'),
-              ('http://english.lokaantar.com/feed/', 'lk'),
-              ('https://www.nepalisansar.com/feed/', 'ns'),
-              ('http://telegraphnepal.com/feed/', 'tn'),
-              ]
+newspapers = [
+    ('https://thehimalayantimes.com/feed/', 'ht'),
+    ('https://english.onlinekhabar.com/feed', 'ok'),
+    ('https://www.nepalitimes.com/feed/', 'nt'),
+    ('https://kathmandutribune.com/feed/', 'kt'),
+    ('http://english.lokaantar.com/feed/', 'lk'),
+    ('https://www.nepalisansar.com/feed/', 'ns'),
+    ('http://telegraphnepal.com/feed/', 'tn'),
+    ]
 
-stop_words = set(stopwords.words('english'))      
+stop_words = set(stopwords.words('english'))
 
-data_level1=['arabl land','avail data','bureau statist','busi survey','cens publi','cens pop','children employ','civil registr','collect method','commerci export','complet rate','consum electr','consum energ','data access','data collect','data compil','data entri','data manag','data releas','data standard','data user','demograph data','densit popul','develop data','difusion dat','direct statist','disaggreg data','electr access','electr consumpt','energi consumpt','establish survey','exchang rate','extern debt','fertil rate','food import','food product','gender gap','govern debt','govern statist','gross domest','gross nation','health expenditur','health survey','import marchandis','improv data','improv statist','indic measur','indic preci','inflat rate','institut statist','interest payment','intern tourism','irrig land','land use','life expect','livestock product','merchandis export','merchandis trade','model statist','mortal rate','multilater debt','nation account','nation statist','nation survey','national brut','national statist','open data','part revenus','pay gap','popul census','popul growth','popul rate','price index','produccion aliment','purchas power','qualiti data','receit fiscal','releas data','revenu fiscal','rural popul','servic export','statist agenc','statist author','statist avail','statist committe','statist data','statist depart','statist national','statist offic','statist servic','statist studi','survey catalogu','tax payment','tax revenu','trade balanc','unemploy rate','use data','water suppli','youth unemploy']
+data_level1 = [
+    'arabl land',
+    'avail data',
+    'bureau statist',
+    'busi survey',
+    'cens publi',
+    'cens pop',
+    'children employ',
+    'civil registr',
+    'collect method',
+    'commerci export',
+    'complet rate',
+    'consum electr',
+    'consum energ',
+    'data access',
+    'data collect',
+    'data compil',
+    'data entri',
+    'data manag',
+    'data releas',
+    'data standard',
+    'data user',
+    'demograph data',
+    'densit popul',
+    'develop data',
+    'difusion dat',
+    'direct statist',
+    'disaggreg data',
+    'electr access',
+    'electr consumpt',
+    'energi consumpt',
+    'establish survey',
+    'exchang rate',
+    'extern debt',
+    'fertil rate',
+    'food import',
+    'food product',
+    'gender gap',
+    'govern debt',
+    'govern statist',
+    'gross domest',
+    'gross nation',
+    'health expenditur',
+    'health survey',
+    'import marchandis',
+    'improv data',
+    'improv statist',
+    'indic measur',
+    'indic preci',
+    'inflat rate',
+    'institut statist',
+    'interest payment',
+    'intern tourism',
+    'irrig land',
+    'land use',
+    'life expect',
+    'livestock product',
+    'merchandis export',
+    'merchandis trade',
+    'model statist',
+    'mortal rate',
+    'multilater debt',
+    'nation account',
+    'nation statist',
+    'nation survey',
+    'national brut',
+    'national statist',
+    'open data',
+    'part revenus',
+    'pay gap',
+    'popul census',
+    'popul growth',
+    'popul rate',
+    'price index',
+    'produccion aliment',
+    'purchas power',
+    'qualiti data',
+    'receit fiscal',
+    'releas data',
+    'revenu fiscal',
+    'rural popul',
+    'servic export',
+    'statist agenc',
+    'statist author',
+    'statist avail',
+    'statist committe',
+    'statist data',
+    'statist depart',
+    'statist national',
+    'statist offic',
+    'statist servic',
+    'statist studi',
+    'survey catalogu',
+    'tax payment',
+    'tax revenu',
+    'trade balanc',
+    'unemploy rate',
+    'use data',
+    'water suppli',
+    'youth unemploy',
+    ]
 
-data_level2=['accur','adequ','ambigu','ambígu','apropi','bancal','bias','confiabl','correct','deceit','deceiv','decept','defectu','delud','engan','equivoc','erreur','erro','erron','errone','error','exact','exat','fake','fallaci','faux','fiabl','generaliz','illus','imparcial','impartial','imprecis','improp','inaccur','incorrect','inexact','invalid','limit','manipul','mislead','mistaken','parcial','prec','precis','proper','reliabl','rigor','rigour','scientif','sol','solid','som','son','sound','spurious','tromp','trompeur','unbias','unreli','unscientif','unsound','vag','vagu','val','valid',]
+data_level2 = [
+    'accur',
+    'adequ',
+    'ambigu',
+    'amb\xc3\xadgu',
+    'apropi',
+    'bancal',
+    'bias',
+    'confiabl',
+    'correct',
+    'deceit',
+    'deceiv',
+    'decept',
+    'defectu',
+    'delud',
+    'engan',
+    'equivoc',
+    'erreur',
+    'erro',
+    'erron',
+    'errone',
+    'error',
+    'exact',
+    'exat',
+    'fake',
+    'fallaci',
+    'faux',
+    'fiabl',
+    'generaliz',
+    'illus',
+    'imparcial',
+    'impartial',
+    'imprecis',
+    'improp',
+    'inaccur',
+    'incorrect',
+    'inexact',
+    'invalid',
+    'limit',
+    'manipul',
+    'mislead',
+    'mistaken',
+    'parcial',
+    'prec',
+    'precis',
+    'proper',
+    'reliabl',
+    'rigor',
+    'rigour',
+    'scientif',
+    'sol',
+    'solid',
+    'som',
+    'son',
+    'sound',
+    'spurious',
+    'tromp',
+    'trompeur',
+    'unbias',
+    'unreli',
+    'unscientif',
+    'unsound',
+    'vag',
+    'vagu',
+    'val',
+    'valid',
+    ]
 
-data_level3=['data manipul','lead question','manipul dat','report bias','sampl select','sampl size']
+data_level3 = [
+    'data manipul',
+    'lead question',
+    'manipul dat',
+    'report bias',
+    'sampl select',
+    'sampl size',
+    ]
 
-data_level_indicator = [' cpi ', ' fdi ', ' gdp ', ' gnp ', ' hdi ', ' wdi ']
+data_level_indicator = [
+    ' cpi ',
+    ' fdi ',
+    ' gdp ',
+    ' gnp ',
+    ' hdi ',
+    ' wdi ',
+    ]
 
-filter_list=[' data ',' record ',' research ',' statistics ',' study ']
+filter_list = [' data ', ' record ', ' research ', ' statistics ',
+               ' study ']
+
 
 def lengths_of_keywords():
-    return len(data_level1), len(data_level2), len(data_level3)
+    return (len(data_level1), len(data_level2), len(data_level3))
+
 
 def cleanHTML(raw_html):
-    text = BeautifulSoup(raw_html, "lxml").text
-    word_tokens = word_tokenize(text.lower().rstrip()) 
-    filtered_sentence = [w for w in word_tokens if not w in stop_words] 
+    text = BeautifulSoup(raw_html, 'lxml').text
+    word_tokens = word_tokenize(text.lower().rstrip())
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
     return ' '.join(filtered_sentence)
+
 
 def performRSSNew(url, newspaper):
     all_links = []
     NewsFeed = feedparser.parse(url)
     entries = NewsFeed.entries
     for entry in entries:
-            content =  cleanHTML(entry.content[0].value)
-            published = entry.published
-            temp_dict = ({'url': entry.link, 
-            'content': content , 
+        content = cleanHTML(entry.content[0].value)
+        published = entry.published
+        temp_dict = {
+            'url': entry.link,
+            'content': content,
             'newspaper': newspaper,
-            'published_date': published})
-            all_links.append(temp_dict)    
+            'published_date': published,
+            }
+        all_links.append(temp_dict)
     return all_links
+
 
 def level1_count(article):
     if article:
         keyword_list = []
         for word in data_level1:
-            search_ = (r"\b"+word.split()[0]+r"[a-zA-Z]*\s\b"+word.split()[1]+"[a-zA-Z]*")
+            search_ = r"\b" + word.split()[0] + r"[a-zA-Z]*\s\b" \
+                + word.split()[1] + r"[a-zA-Z]*"
             if re.search(search_, article):
                 keyword_list.append(word)
         for word in data_level_indicator:
-            if (word in article):
+            if word in article:
                 keyword_list.append(word)
         return keyword_list
     return []
+
 
 def level2_count(article, valid):
-    if valid==1:
+    if valid == 1:
         keyword_list = []
         for word in data_level2:
-            search_ = (r"\b"+word+r"[a-zA-Z]*")
+            search_ = r"\b" + word + r"[a-zA-Z]*"
             if re.search(search_, article):
                 keyword_list.append(word)
         return keyword_list
     return []
 
+
 def level3_count(article, valid):
-    if valid==1:
+    if valid == 1:
         keyword_list = []
         for word in data_level3:
-            search_ = (r"\b"+word+r"[a-zA-Z]*")
+            search_ = r"\b" + word + r"[a-zA-Z]*"
             if re.search(search_, article):
                 keyword_list.append(word)
         return keyword_list
     return []
+
 
 def level_2_3_filter(article):
     for word in filter_list:
@@ -89,46 +281,48 @@ def level_2_3_filter(article):
             return 1
     return 0
 
+
 def level_len(count_list):
     if type(count_list) == str:
-        return 1 if len(count_list)>2 else 0
-    return 1 if len(count_list)>0 else 0
+        return (1 if len(count_list) > 2 else 0)
+    return (1 if len(count_list) > 0 else 0)
 
 
-def fetch_merge_analyze_data_new(reset_analysis = False):
+def fetch_merge_analyze_data_new(reset_analysis=False):
     start = time.time()
     prev_all = pd.read_csv('newspaper/static/datasets/all.csv')
 
     for newspaper in newspapers:
         temp = pd.DataFrame(performRSSNew(newspaper[0], newspaper[1]))
         df_ALL = pd.concat([prev_all, temp], sort=False)
-    
-    df_ALL = df_ALL.drop_duplicates(subset='url', keep="first").reset_index(drop=True) 
-     
-    print ('fetch_data and compile_data', time.time() - start)
-    
-    if (reset_analysis):
+
+    df_ALL = df_ALL.drop_duplicates(subset='url', keep='first'
+                                    ).reset_index(drop=True)
+
+    if reset_analysis:
         df_ALL['level1'] = np.nan
         df_ALL['level2'] = np.nan
         df_ALL['level3'] = np.nan
-        print ('reset_data', time.time() - start)
-    
- 
-    df_ALL['level1'] = df_ALL.apply(lambda x: level1_count(x['content']) if pd.isnull(x.level1) else x.level1, axis=1)
+
+    df_ALL['level1'] = df_ALL.apply(lambda x: (level1_count(x['content'
+                                    ]) if pd.isnull(x.level1) else x.level1),
+                                    axis=1)
     df_ALL['level_len'] = df_ALL['level1'].apply(level_len)
-    
-    print ('level_1_analysis', time.time() - start)
-    
-    df_ALL['level_2_3_valid'] = df_ALL['content'].apply(level_2_3_filter)
-    print ('level_2_filter ', time.time() - start)
-    
-    df_ALL['level2'] =  df_ALL.apply(lambda x: level2_count(x['content'], x['level_2_3_valid']) if pd.isnull(x.level2) else x.level2, axis=1)
+
+    df_ALL['level_2_3_valid'] = df_ALL['content'
+            ].apply(level_2_3_filter)
+
+    df_ALL['level2'] = df_ALL.apply(lambda x: (level2_count(x['content'
+                                    ], x['level_2_3_valid'
+                                    ]) if pd.isnull(x.level2) else x.level2),
+                                    axis=1)
     df_ALL['level2_len'] = df_ALL.level2.apply(level_len)
-    print ('level_2_analysis ', time.time() - start)
-    
-    df_ALL['level3'] =  df_ALL.apply(lambda x: level3_count(x['content'], x['level_2_3_valid']) if pd.isnull(x.level3) else x.level3, axis=1)
+
+    df_ALL['level3'] = df_ALL.apply(lambda x: (level3_count(x['content'
+                                    ], x['level_2_3_valid'
+                                    ]) if pd.isnull(x.level3) else x.level3),
+                                    axis=1)
     df_ALL['level3_len'] = df_ALL.level3.apply(level_len)
-    print ('level_3_analysis ', time.time() - start)
-    
+
     df_ALL.to_csv('newspaper/static/datasets/all.csv', index=False)
     return df_ALL
--- newspaper/routes.py
+++ newspaper/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template, request
 from newspaper import application
 import pandas as pd
-from newspaper.fetch_data import fetch_merge_analyze_data_new, lengths_of_keywords
-from newspaper.level_chart import create_level, create_level_percent, create_pie_chart
+from newspaper.fetch_data import fetch_merge_analyze_data_new, \
+    lengths_of_keywords
+from newspaper.level_chart import create_level, create_level_percent, \
+    create_pie_chart
 from apscheduler.schedulers.background import BackgroundScheduler
 import logging
 import zipfile
@@ -23,41 +25,72 @@ sched_daily = BackgroundScheduler(daemon=True)
 def index():
 
     df = pd.read_csv('newspaper/static/datasets/all.csv')
-    df = df.groupby('newspaper').sum().join(df.groupby('newspaper').size().to_frame('News Articles'))
-    df.rename({'level_len': 'Level1', 'level2_len': 'Level2', 'level3_len': 'Level3', 'level_2_3_valid':'Filtered Articles'}, inplace=True, axis=1)
+    df = df.groupby('newspaper').sum().join(df.groupby('newspaper'
+            ).size().to_frame('News Articles'))
+    df.rename({
+        'level_len': 'Level1',
+        'level2_len': 'Level2',
+        'level3_len': 'Level3',
+        'level_2_3_valid': 'Filtered Articles',
+        }, inplace=True, axis=1)
 
-    df['Level1 %'] = (100*df['Level1']/df['News Articles']).map('{:,.1f} %'.format)
-    df['Level2 %'] = (100*df['Level2']/df['Filtered Articles']).map('{:,.1f} %'.format)
-    df['Level3 %'] = (100*df['Level3']/df['Filtered Articles']).map('{:,.1f} %'.format)
+    df['Level1 %'] = (100 * df['Level1'] / df['News Articles'
+                      ]).map('{:,.1f} %'.format)
+    df['Level2 %'] = (100 * df['Level2'] / df['Filtered Articles'
+                      ]).map('{:,.1f} %'.format)
+    df['Level3 %'] = (100 * df['Level3'] / df['Filtered Articles'
+                      ]).map('{:,.1f} %'.format)
 
-
-    mapping = pd.DataFrame({'Newspaper': ['The Himalayan Times', 'Katmandu Tribune', 'Lokaantar', 'Nepali Sansar', 'Nepali Times', 'Online Khabar', 'Telegraph Nepal']}, 
-             index=['ht', 'kt', 'lk', 'ns', 'nt', 'ok', 'tn'])
+    mapping = pd.DataFrame({'Newspaper': [
+        'The Himalayan Times',
+        'Katmandu Tribune',
+        'Lokaantar',
+        'Nepali Sansar',
+        'Nepali Times',
+        'Online Khabar',
+        'Telegraph Nepal',
+        ]}, index=[
+        'ht',
+        'kt',
+        'lk',
+        'ns',
+        'nt',
+        'ok',
+        'tn',
+        ])
 
     df = df.join(mapping)
 
-    len_data_level_1, len_data_level_2, len_data_level_3 = lengths_of_keywords()
+    (len_data_level_1, len_data_level_2, len_data_level_3) = \
+        lengths_of_keywords()
     newspapers = df['Newspaper'].tolist()
 
+    chart1 = [{'name': 'Level 1', 'data': df['Level1'].tolist()},
+              {'name': 'Total Articles', 'data': df['News Articles'
+              ].tolist()}]
+    chart2 = (df['Level1'] / df['News Articles'] * 100).tolist()
 
-    chart1 = [{'name':'Level 1','data':df['Level1'].tolist()},{'name':'Total Articles','data':df['News Articles'].tolist()}]
-    chart2 = (df['Level1']/df['News Articles']*100).tolist()
+    chart3 = [{'name': 'Level 2', 'data': df['Level2'].tolist()},
+              {'name': 'Total Articles', 'data': df['News Articles'
+              ].tolist()}]
+    chart4 = (df['Level2'] / df['News Articles'] * 100).tolist()
 
-    chart3 = [{'name':'Level 2','data':df['Level2'].tolist()},{'name':'Total Articles','data':df['News Articles'].tolist()}]
-    chart4 = (df['Level2']/df['News Articles']*100).tolist()
+    chart5 = [{'name': 'Level 3', 'data': df['Level3'].tolist()},
+              {'name': 'Total Articles', 'data': df['News Articles'
+              ].tolist()}]
+    chart6 = (df['Level3'] / df['News Articles'] * 100).tolist()
 
-    chart5 = [{'name':'Level 3','data':df['Level3'].tolist()},{'name':'Total Articles','data':df['News Articles'].tolist()}]
-    chart6 = (df['Level3']/df['News Articles']*100).tolist()
-    
     level1 = df[['Newspaper', 'Level1', 'News Articles', 'Level1 %']]
     column_names_level1 = level1.columns.values
     row_data_level1 = list(level1.values.tolist())
 
-    level2 = df[['Newspaper', 'Level2', 'Filtered Articles', 'News Articles',  'Level2 %']]
+    level2 = df[['Newspaper', 'Level2', 'Filtered Articles',
+                'News Articles', 'Level2 %']]
     column_names_level2 = level2.columns.values
     row_data_level2 = list(level2.values.tolist())
 
-    level3 = df[['Newspaper', 'Level3', 'Filtered Articles', 'News Articles', 'Level3 %']]
+    level3 = df[['Newspaper', 'Level3', 'Filtered Articles',
+                'News Articles', 'Level3 %']]
     column_names_level3 = level3.columns.values
     row_data_level3 = list(level3.values.tolist())
 
@@ -69,46 +102,54 @@ def index():
         'chart4': chart4,
         'chart5': chart5,
         'chart6': chart6,
-        'column_names_level1' : column_names_level1,
-        'row_data_level1' : row_data_level1,
-        'column_names_level2' : column_names_level2,
-        'row_data_level2' : row_data_level2,
-        'column_names_level3' : column_names_level3,
-        'row_data_level3' : row_data_level3,
-    }
+        'column_names_level1': column_names_level1,
+        'row_data_level1': row_data_level1,
+        'column_names_level2': column_names_level2,
+        'row_data_level2': row_data_level2,
+        'column_names_level3': column_names_level3,
+        'row_data_level3': row_data_level3,
+        }
 
-    return render_template ('base.html', **content)
-
+    return render_template('base.html', **content)
 
 
 # This route is created so that in cases of emergencies
 # the data can be synced manually.
+
 @application.route('/scheduler/main_job')
 @basic_auth.required
 def main_job_manual():
+
     # This if for test
     # return get_random_df()
-    if (fetch_merge_analyze_data_new()):
-        return "done"
-    return "check logs"
+
+    if fetch_merge_analyze_data_new():
+        return 'done'
+    return 'check logs'
+
 
 def main_job():
+
     # This if for test
     # return get_random_df()
-    if (fetch_merge_analyze_data_new()):
-        return "done"
-    return "check logs"
+
+    if fetch_merge_analyze_data_new():
+        return 'done'
+    return 'check logs'
+
 
 # TODO check for conflicts
+
 @application.route('/scheduler/start')
 @basic_auth.required
 def start_scheduler():
     try:
-        sched_daily.add_job(main_job, 'interval', minutes = 180, id='get_csvs')
+        sched_daily.add_job(main_job, 'interval', minutes=180,
+                            id='get_csvs')
         sched_daily.start()
-        return ("start collecting csvs")
+        return 'start collecting csvs'
     except:
-        return ("The process might have already started")
+        return 'The process might have already started'
 
 
 @application.route('/scheduler/stop')
@@ -116,18 +157,20 @@ def start_scheduler():
 def stop_scheduler():
     try:
         sched_daily.remove_job('get_csvs')
-        return ("stopped")
+        return 'stopped'
     except:
-        return ("Job not started or already stopped.")
+        return 'Job not started or already stopped.'
+
 
 @application.route('/scheduler/status')
 @basic_auth.required
 def monitor_daemon():
     try:
-        chron_status = str(sched_daily.get_job('get_csvs')) 
+        chron_status = str(sched_daily.get_job('get_csvs'))
         return chron_status
     except:
-        return ('Check the scheduler')
+        return 'Check the scheduler'
+
 
 @application.route('/raw_datasets')
 @basic_auth.required
@@ -138,9 +181,7 @@ def request_zip():
         for f_name in base_path.iterdir():
             z.write(f_name)
     data.seek(0)
-    return flask.send_file(
-        data,
-        mimetype='application/zip',
-        as_attachment=True,
-        attachment_filename=date.today().strftime("%B_%d")+'_datasets.zip'
-    )
+    return flask.send_file(data, mimetype='application/zip',
+                           as_attachment=True,
+                           attachment_filename=date.today().strftime('%B_%d'
+                           ) + '_datasets.zip')
